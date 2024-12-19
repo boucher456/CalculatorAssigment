06fc1b919f60fb4105082b637f4b6e1cd44199ef
boucher456 <amayjagdale456@gmail.com>
2024-12-19 18:11:53 +0530

    |First Test Case|Ameya|

--- src/Test/SimCalculatorJunit.java
+++ src/Test/SimCalculatorJunit.java
@@ -4,11 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import calculator.SimCalculator;
+
 class SimCalculatorJunit {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testAddEmptyString() {
+		SimCalculator calculator = new SimCalculator();
+		assertEquals(0, calculator.add(""));
 	}
 
 }
--- src/calculator/SimCalculator.java
+++ src/calculator/SimCalculator.java
@@ -1,10 +1,16 @@
 package calculator;
 
 public class SimCalculator {
+	public int add(String numbers) {
+		if (numbers.isEmpty()) {
+			return 0;
+		}
+		return -1; // Placeholder for other logic
+	}
 
 	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
 
 	}
-
 }
